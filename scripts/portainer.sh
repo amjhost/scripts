65d6df23cc23db1770f96b619d06468429ed6a16
Amjad Ali <achuachu727@gmail.com>
2024-05-12 23:25:02 +0000

    version 1.0.2

--- scripts/portainer.sh
+++ scripts/portainer.sh
@@ -3,26 +3,28 @@
 # You need to run this script with root privilages.
 
 #Script tested on ubuntu 22.04 
-echo "##########################           Script tested on ubuntu 22.04#                ################################### "
+echo "##########################Script tested on ubuntu 22.04#################################### "
 
 
 # updating the packages and upgrading, installing docker, python and docker-compose
-echo "####################       Updating the packages and upgrading, installing docker, python and docker-compose         #########################"
+echo "updating the packages and upgrading, installing docker, python and docker-compose"
 
 apt update -y && apt upgrade -y && apt install docker.io  python3-pip -y && pip install docker-compose
 ##Installation of Selfhostedpro yacht
 
-echo"#################         Installation of Selfhostedpro yacht            ##########################"
+echo"Installation of Selfhostedpro yacht"
 
 docker volume create yacht
 docker run -d -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config --name yacht --restart unless-stopped selfhostedpro/yacht
 
 ##installation of portainer
 
+echo "###################             installation of portainer                ###################"
+
 docker volume create portainer_data
 docker run -d -p 8001:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 ## automatic Installation of code-server
-echo "Automatic Installation of code-server"
+echo "#######################          Automatic Installation of code-server             ##########################"
 
 curl -fsSL https://code-server.dev/install.sh | sh
 
@@ -44,7 +46,7 @@ systemctl restart code-server@root.service
 
 # Installation of cloud panel
 
-echo "Installation of cloudpanel....."
+echo "I##################################          Installation of cloudpanel.....             ##################################"
 
 curl -sS https://installer.cloudpanel.io/ce/v2/install.sh -o install.sh; \
 echo "85762db0edc00ce19a2cd5496d1627903e6198ad850bbbdefb2ceaa46bd20cbd install.sh" | \
