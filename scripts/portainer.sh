4fbb3315a698a59a894964f5427eab804cae4534
Amjad Ali <achuachu727@gmail.com>
2024-05-12 21:25:41 +0000

    version 1

--- scripts/portainer.sh
+++ scripts/portainer.sh
@@ -15,7 +15,7 @@ docker run -d -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v yacht
 ##installation of portainer
 
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker run -d -p 8001:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 ### Automatic Installation of code-server
 
 curl -fsSL https://code-server.dev/install.sh | sh
