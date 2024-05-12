7aceb4642a7854ede7b75a7316d8b16861334304
Amjad Ali <achuachu727@gmail.com>
2024-05-12 19:50:32 +0000

    version 0.1

--- scripts/portainer.sh
+++ scripts/portainer.sh
@@ -12,6 +12,11 @@ apt update -y && apt upgrade -y && apt install docker.io  python3-pip -y && pip 
 docker volume create yacht
 docker run -d -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock -v yacht:/config --name yacht --restart unless-stopped selfhostedpro/yacht
 
+##installation of portainer
+
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
 ###installation of cloudpanel
 
 curl -sS https://installer.cloudpanel.io/ce/v2/install.sh -o install.sh; \
