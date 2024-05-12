5586d4f19ac31e4a30fd6e1a9f1549d5cd1e0944
Amjad Ali <achuachu727@gmail.com>
2024-05-12 21:56:02 +0000

    test

--- scripts/portainer.sh
+++ scripts/portainer.sh
@@ -27,6 +27,6 @@ printf "bind-addr: 0.0.0.0:8080\nauth: password\npassword: Amjadali@2021\ncert: 
 
 ###installation of cloudpanel
 
-curl -sS https://installer.cloudpanel.io/ce/v2/install.sh -o install.sh; \
-echo "85762db0edc00ce19a2cd5496d1627903e6198ad850bbbdefb2ceaa46bd20cbd install.sh" | \
-sha256sum -c && sudo DB_ENGINE=MARIADB_10.11 bash install.sh
+#curl -sS https://installer.cloudpanel.io/ce/v2/install.sh -o install.sh; \
+#echo "85762db0edc00ce19a2cd5496d1627903e6198ad850bbbdefb2ceaa46bd20cbd install.sh" | \
+#sha256sum -c && sudo DB_ENGINE=MARIADB_10.11 bash install.sh
